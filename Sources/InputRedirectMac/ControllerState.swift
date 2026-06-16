09ba1fad1b473dd9f58a03a02365e2aed9347825
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-16 18:00:43 -0300

    Update ControllerState.swift

--- Sources/InputRedirectMac/ControllerState.swift
+++ Sources/InputRedirectMac/ControllerState.swift
@@ -1,1 +1,20 @@
+import Foundation
 
+struct ControllerState {
+
+    var a = false
+    var b = false
+    var x = false
+    var y = false
+
+    var l = false
+    var r = false
+
+    var start = false
+    var select = false
+
+    var up = false
+    var down = false
+    var left = false
+    var right = false
+}
